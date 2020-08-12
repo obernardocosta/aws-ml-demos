41acb413fa4ca88ec4d5d5edf9efd5a7c9201c27
Bernardo Costa <bernardoantunescosta@gmail.com>
2020-08-12 13:19:38 -0300

    Create conf.py

---
+++ 2.0-amazon-sagemaker/2.2-hardhat-object-detection/conf.py
@@ -0,0 +1,56 @@
+import uuid
+
+
+SESSION_BUCKET = '<YOUR_BUCKET>'
+
+raw_annotation_path = 'GDUT-HWD/Annotations'
+test_image_base_path = 'img/raw'
+detection_image_base_path = 'img/detection'
+
+# Replace before CreateEndpoint
+training_job_name = 'object-detection-2020-08-11-21-55-56-208'  
+
+# Replace before CreateEndpoint
+model_data = 's3://{}/output/object-detection-2020-08-11-21-55-56-208/output/model.tar.gz'.format(SESSION_BUCKET)
+
+endpoint_name = training_job_name
+model_name = 'object-detector-hardhat-200ep'
+
+container_image = '811284229777.dkr.ecr.us-east-1.amazonaws.com/object-detection:latest'
+
+# Endpoint instance
+deploy_instance_type = 'ml.m4.xlarge'
+initial_instance_count = 1
+
+# Classes
+object_categories = ['none', 'red', 'yellow', 'white', 'blue']
+class_colors = ['grey', 'red', 'yellow', 'white', 'blue']
+
+
+# Trainning Conf
+train_instance_count=1
+train_instance_type='ml.p3.2xlarge'
+train_volume_size = 50
+train_use_spot_instances=True
+train_max_wait=360000
+train_max_run = 360000
+input_mode = 'File'
+
+
+# Hyperparameters
+base_network='resnet-50'
+use_pretrained_model=1
+num_classes=len(object_categories)
+mini_batch_size=16
+epochs=200
+learning_rate=0.001
+lr_scheduler_step='3,6'
+lr_scheduler_factor=0.1
+optimizer='sgd'
+momentum=0.9
+weight_decay=0.0005
+overlap_threshold=0.5
+nms_threshold=0.45
+image_shape=512
+label_width=1600
+num_training_samples=2539
